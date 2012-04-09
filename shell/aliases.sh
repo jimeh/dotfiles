14d141e1c487768d0a7d09403a33be5cd83c8fdb
Jim Myhrberg <contact@jimeh.me>
2012-04-09 03:33:17 +0100

    Add shell alias for weechat setting TERM to a value that works

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -19,5 +19,6 @@ alias br="brew"
 alias devnullsmtp="java -jar $DOTBIN/DevNullSmtp.jar"
 
 # Misc.
+alias weechat="TERM=screen-256colors weechat-curses"
 alias slashdot="ab -kc 50 -t 300"
 alias digg="ab -kc 50 -t 30"
