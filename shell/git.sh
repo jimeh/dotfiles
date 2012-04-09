dd14c9d12f53b1e8345b28865d1c7b676717c896
Jim Myhrberg <contact@jimeh.me>
2012-04-09 03:34:22 +0100

    Ensure GIT_AUTHOR_* environment vars are set correctly

--- shell/git.sh
+++ shell/git.sh
@@ -3,8 +3,8 @@
 #
 
 # Author name
-export GIT_AUTHOR_NAME="`git config user.name`"
-export GIT_AUTHOR_EMAIL="`git config user.email`"
+export GIT_AUTHOR_NAME="`git config --global user.name`"
+export GIT_AUTHOR_EMAIL="`git config --global user.email`"
 
 # Aliases
 alias g="git"
