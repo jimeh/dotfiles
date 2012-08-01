58517f994790e1f64097ad99a399f6ba20a9ff95
Jim Myhrberg <contact@jimeh.me>
2012-08-01 09:21:54 +0100

    Load shell-completion for nvm if file exists

--- shell/nodejs.sh
+++ shell/nodejs.sh
@@ -5,4 +5,9 @@
 # Load nvm if it's available
 if [ -f "$HOME/.nvm/nvm.sh" ]; then
   source "$HOME/.nvm/nvm.sh"
+
+  # And it's shell completion
+  if [ -f "$HOME/.nvm/bash_completion" ]; then
+    source "$HOME/.nvm/bash_completion"
+  fi
 fi
