33aeb11b38f31d6ffe05d4895d1a6dca793e103e
Jim Myhrberg <contact@jimeh.me>
2012-08-01 09:21:36 +0100

    Setup environment for Homebrew supplied Android SDK

--- shell/_main.sh
+++ shell/_main.sh
@@ -51,6 +51,9 @@ export LANG="en_US.UTF-8"
 # GCC 4.2 via Homebrew
 export CC=/usr/local/bin/gcc-4.2
 
+# Android SDK
+export ANDROID_SDK_ROOT="/usr/local/Cellar/android-sdk/r20.0.1"
+
 # ensure bin and sbin paths from /usr/local are in PATH
 path_add_after "/usr/local/sbin" "/usr/local/bin"
 
