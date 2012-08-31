1fdd7fb1d857ceb0082f89dfdd7ec296a98a4e0c
Jim Myhrberg <contact@jimeh.me>
2012-08-31 13:29:51 +0300

    Move shell environment setup into env.sh

---
+++ shell/env.sh
@@ -0,0 +1,36 @@
+#
+# Environment Setup
+#
+
+# Editors
+export EDITOR="emacsclient-wrapper"
+export GEM_EDITOR="mate"
+
+# Locale Setup
+export LC_ALL="en_US.UTF-8"
+export LANG="en_US.UTF-8"
+
+# GCC 4.2 via Homebrew
+export CC=/usr/local/bin/gcc-4.2
+
+# Android SDK
+export ANDROID_SDK_ROOT="/usr/local/Cellar/android-sdk/r20.0.1"
+
+# ensure bin and sbin paths from /usr/local are in PATH
+path_add_after "/usr/local/sbin" "/usr/local/bin"
+
+# Add user's bin directory to PATH
+path_prepend "$HOME/bin"
+
+# Add dotfiles' bin directory to PATH
+path_prepend "$DOTBIN"
+
+# Relative Paths - must be first in PATH
+path_prepend "./node_modules/.bin" # Node.js
+path_prepend "./vendor/bundle/bin" # Ruby Bundler
+
+# Ensure TMPDIR is the same for local and remote ssh logins
+if [[ $TMPDIR == "/var/folders/"* ]] || [[ $TMPDIR == "" ]]; then
+  export TMPDIR="/tmp/user-$USER"
+  mkdir -p "$TMPDIR"
+fi
