5efd93a29ae8832e8db7c603894d5c6157dffff4
Jim Myhrberg <contact@jimeh.me>
2012-09-11 09:34:00 +0100

    Update Tmux-related shell config

--- shell/tmux.sh
+++ shell/tmux.sh
@@ -3,6 +3,7 @@ alias tm="tmux"
 alias tma="tm att"
 alias tmn="tm new"
 alias tml="tm ls"
+alias tmm="tm -s main"
 
 # Tmux Completion
 if [ -f "/usr/local/etc/bash_completion.d/tmux" ]; then
@@ -10,11 +11,12 @@ if [ -f "/usr/local/etc/bash_completion.d/tmux" ]; then
 fi
 
 # Tmuxifier
-export TMUXIFIER="$DOTSHELL/tmux/tmuxifier"
-export TMUXIFIER_LAYOUT_PATH="$DOTFILES/private/tmux-layouts"
+if [ -f "$DOTSHELL/tmux/tmuxifier" ]; then
+  export TMUXIFIER="$DOTSHELL/tmux/tmuxifier"
+  export TMUXIFIER_LAYOUT_PATH="$DOTFILES/private/tmux-layouts"
+  [[ -s "$TMUXIFIER/init.sh" ]] && source "$TMUXIFIER/init.sh"
 
-[[ -s "$TMUXIFIER/init.sh" ]] && source "$TMUXIFIER/init.sh"
-
-alias m="tmuxifier"
-alias ms="tmuxifier load-session"
-alias mw="tmuxifier load-window"
+  alias m="tmuxifier"
+  alias ms="tmuxifier load-session"
+  alias mw="tmuxifier load-window"
+fi
