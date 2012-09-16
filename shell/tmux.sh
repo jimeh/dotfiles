16854318402001163b3874bd4f8c907cb10e1e7c
Jim Myhrberg <contact@jimeh.me>
2012-09-16 20:13:48 +0100

    Fixed incorrect tmux alias

--- shell/tmux.sh
+++ shell/tmux.sh
@@ -3,7 +3,7 @@ alias tm="tmux"
 alias tma="tm att"
 alias tmn="tm new"
 alias tml="tm ls"
-alias tmm="tm -s main"
+alias tmm="tmn -s main"
 
 # Tmux Completion
 if [ -f "/usr/local/etc/bash_completion.d/tmux" ]; then
