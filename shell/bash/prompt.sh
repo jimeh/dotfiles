ac6cc9b83c1764be4d9c0eddaca464e02305687f
Jim Myhrberg <contact@jimeh.me>
2013-04-15 21:30:23 +0100

    Small tweak to bash prompt

--- shell/bash/prompt.sh
+++ shell/bash/prompt.sh
@@ -6,7 +6,7 @@
 if [ -f "$DOTBASH/git-aware-prompt/main.sh" ]; then
   GITAWAREPROMPT="$DOTBASH/git-aware-prompt"
   source "$GITAWAREPROMPT/main.sh"
-  export PS1="\u@\h \w\[$txtcyn\]\$git_branch\[$txtylw\]\$git_dirty\[$txtrst\]\$ "
+  export PS1="\u@\h \w \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "
   export SUDO_PS1="\[$bakred\]\u@\h\[$txtrst\] \w\$ "
 else
   # Customize prompt to act like pre-leopard
