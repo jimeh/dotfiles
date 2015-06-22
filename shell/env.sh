9e4e1ef08291027ee20ebf39cd21ec1be1f21af6
Jim Myhrberg <contact@jimeh.me>
2015-06-23 00:46:55 +0100

    Update environment vars

--- shell/env.sh
+++ shell/env.sh
@@ -11,10 +11,11 @@ export LC_ALL="en_US.UTF-8"
 export LANG="en_US.UTF-8"
 
 # GCC 4.2 via Homebrew
-export CC=/usr/local/bin/gcc-4.2
+# export CC=/usr/local/bin/gcc-4.2
 
 # Android SDK
 export ANDROID_SDK_ROOT="/usr/local/Cellar/android-sdk/r20.0.1"
+export ANDROID_HOME="/usr/local/opt/android-sdk"
 
 # ensure bin and sbin paths from /usr/local are in PATH
 path_add_after "/usr/local/sbin" "/usr/local/bin"
