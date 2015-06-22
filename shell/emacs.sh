693a06bf87d35e2ae2a35d9b8be1201d2a67685a
Jim Myhrberg <contact@jimeh.me>
2015-06-23 00:46:23 +0100

    Update emacs env setup

--- shell/emacs.sh
+++ shell/emacs.sh
@@ -2,9 +2,16 @@
 # Emacs
 #
 
+# OS X systems.
 if [ -f "/Applications/Emacs.app/Contents/MacOS/Emacs" ]; then
   alias emacs="/Applications/Emacs.app/Contents/MacOS/Emacs -nw"
 fi
+
 if [ -f "/Applications/Emacs.app/Contents/MacOS/bin/emacsclient" ]; then
   alias emacsclient="/Applications/Emacs.app/Contents/MacOS/bin/emacsclient"
+fi
+
+# *nix systems.
+if [ -d "/opt/emacs/bin" ]; then
+  path_append "/opt/emacs/bin"
 fi
