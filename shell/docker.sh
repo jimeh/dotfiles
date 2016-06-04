25f2ca12943c070a1541dad4b5ad02407f45d403
Jim Myhrberg <contact@jimeh.me>
2016-06-05 00:25:02 +0100

    Add docker related shell customizations

---
+++ shell/docker.sh
@@ -0,0 +1,9 @@
+#
+# Docker Related
+#
+
+alias d="docker"
+alias co="docker-compose"
+
+alias redis-cli="docker run -it --net=host --rm redis redis-cli"
+alias mysql="docker run -it --net=host --rm mysql mysql"
