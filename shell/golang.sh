0107df33359b7aff6da182c598b051ac2ac38340
Jim Myhrberg <contact@jimeh.me>
2016-07-09 22:52:39 +0100

    Add common Go dependency managers to default pkg list

--- shell/golang.sh
+++ shell/golang.sh
@@ -7,8 +7,11 @@ path_prepend "$HOME/Projects/Go/bin:$HOME/.go/bin"
 
 install_go_global_packages () {
   local packages=( \
+    github.com/FiloSottile/gvt \
+    github.com/Masterminds/glide \
     github.com/alecthomas/gometalinter \
     github.com/golang/lint/golint \
+    github.com/kardianos/govendor \
     github.com/kisielk/errcheck \
     github.com/kr/pretty \
     github.com/laher/goxc \
