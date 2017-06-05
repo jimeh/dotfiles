a6efb7c4e56283515ee572495ddfff418fd32d28
Jim Myhrberg <contact@jimeh.me>
2017-06-05 23:23:06 +0100

    Fix indentation

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -74,26 +74,26 @@ pman () {
 # Extract most common archives with single command.
 #  - from: http://alias.sh/extract-most-know-archives-one-command
 extract() {
-    if [ -f $1 ] ; then
-      case $1 in
-        *.tar.bz2)   tar xvjf $1    ;;
-        *.tar.gz)    tar xvzf $1    ;;
-        *.tar.xz)    tar xvJf $1    ;;
-        *.bz2)       bunzip2 $1     ;;
-        *.rar)       unrar e $1     ;;
-        *.gz)        gunzip $1      ;;
-        *.tar)       tar xvf $1     ;;
-        *.tbz2)      tar xvjf $1    ;;
-        *.tbz)       tar xvjf $1    ;;
-        *.tgz)       tar xvzf $1    ;;
-        *.txz)       tar xvJf $1    ;;
-        *.zip)       unzip $1       ;;
-        *.Z)         uncompress $1  ;;
-        *.7z)        7z x $1        ;;
-        *)     echo "'$1' cannot be extracted via extract()" ;;
-         esac
-     else
-         echo "'$1' is not a valid file"
-     fi
+  if [ -f $1 ] ; then
+    case $1 in
+      *.tar.bz2)   tar xvjf $1    ;;
+      *.tar.gz)    tar xvzf $1    ;;
+      *.tar.xz)    tar xvJf $1    ;;
+      *.bz2)       bunzip2 $1     ;;
+      *.rar)       unrar e $1     ;;
+      *.gz)        gunzip $1      ;;
+      *.tar)       tar xvf $1     ;;
+      *.tbz2)      tar xvjf $1    ;;
+      *.tbz)       tar xvjf $1    ;;
+      *.tgz)       tar xvzf $1    ;;
+      *.txz)       tar xvJf $1    ;;
+      *.zip)       unzip $1       ;;
+      *.Z)         uncompress $1  ;;
+      *.7z)        7z x $1        ;;
+      *)           echo "'$1' cannot be extracted via extract()" ;;
+    esac
+  else
+    echo "'$1' is not a valid file"
+  fi
 }
 alias ext=extract
