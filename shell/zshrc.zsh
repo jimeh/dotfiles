a1cd179ac889114cbafe68c304cb891029f77017
Jim Myhrberg <contact@jimeh.me>
2017-07-31 12:42:22 +0100

    Pull my zsh theme from github instead of locally from the dotfiles

--- shell/zshrc.zsh
+++ shell/zshrc.zsh
@@ -32,7 +32,7 @@ zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-syntax-highlighting", defer:3
 
 # zplug "aaronjamesyoung/aaron-zsh-theme", as:theme
-zplug "$DOTZSH/themes/plain", from:local, as:theme
+zplug "jimeh/plain.zsh-theme", as:theme
 
 # Install plugins if there are plugins that have not been installed
 if ! zplug check --verbose; then
