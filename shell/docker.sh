992332807991f0de1fe5da1247c3d54a71b9266e
Jim Myhrberg <contact@jimeh.me>
2017-10-02 13:56:27 +0100

    Update docker-related shell aliases

--- shell/docker.sh
+++ shell/docker.sh
@@ -5,8 +5,8 @@
 alias docker-cloud="docker run -it -v ~/.docker:/root/.docker:ro --rm dockercloud/cli"
 
 alias d="docker"
+alias dc="docker-compose"
 alias co="docker-compose"
-alias dc="docker-cloud"
 
 # alias redis-cli="docker run -it --net=host --rm redis redis-cli"
 # alias mysql="docker run -it --net=host --rm mysql mysql"
