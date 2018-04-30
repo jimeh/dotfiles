46fb55428d0c3c814b555d16ca62a62e231ca248
Jim Myhrberg <contact@jimeh.me>
2018-04-30 15:58:25 +0100

    Set KUBECONFIG environment variable

--- shell/kubernetes.sh
+++ shell/kubernetes.sh
@@ -6,6 +6,8 @@ alias kc="kubectl"
 alias hl="helm"
 alias mk="minikube"
 
+export KUBECONFIG="$HOME/.kube/config:.kube/config"
+
 if command -v kubectl > /dev/null; then
   eval "$(kubectl completion zsh)"
 fi
