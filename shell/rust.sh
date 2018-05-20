e5f8c78518cd33cbe78185a1e9863b65cfc87d84
Jim Myhrberg <contact@jimeh.me>
2018-05-20 11:10:21 +0100

    Add function to install global rust packages

--- shell/rust.sh
+++ shell/rust.sh
@@ -6,3 +6,10 @@
 if [ -d "$HOME/.cargo/bin" ]; then
   path_prepend "$HOME/.cargo/bin"
 fi
+
+install_rust_global_packages() {
+  rustup component add rustfmt-preview
+  rustup component add rust-src
+  cargo +nightly install --force clippy
+  cargo install --force racer
+}
