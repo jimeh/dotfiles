dde980afefb5d06a8c1944596781cce6d2c04b9f
Jim Myhrberg <contact@jimeh.me>
2018-05-30 10:30:45 +0100

    Update hammerspoon keybindings for UAC00024

--- hammerspoon/hosts/UAC00024.lua
+++ hammerspoon/hosts/UAC00024.lua
@@ -8,10 +8,10 @@ function obj.init()
   apptoggle:bind({'cmd', 'ctrl'}, 'C', 'Paw')
   apptoggle:bind({'cmd', 'ctrl'}, 'D', 'Mail')
   apptoggle:bind({'cmd', 'ctrl'}, 'E', 'Emacs', '/Applications/Emacs.app')
-  apptoggle:bind({'cmd', 'ctrl'}, 'G', 'Slack')
+  apptoggle:bind({'cmd', 'ctrl'}, 'G', 'Stride')
   apptoggle:bind({'cmd', 'ctrl'}, 'S', 'Skype for Business')
   apptoggle:bind({'cmd', 'ctrl'}, 'X', 'Calendar')
-  apptoggle:bind({'cmd', 'ctrl'}, 'Z', 'Stride')
+  apptoggle:bind({'cmd', 'ctrl'}, 'Z', 'Microsoft Teams')
 end
 
 return obj
