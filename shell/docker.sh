c841651546c132b55271ea3f2a73c770ef3ac797
Jim Myhrberg <contact@jimeh.me>
2018-07-19 22:45:01 +0100

    Remove unused stuff

--- shell/docker.sh
+++ shell/docker.sh
@@ -6,9 +6,6 @@ alias d="docker"
 alias dc="docker-compose"
 alias co="docker-compose"
 
-# alias redis-cli="docker run -it --net=host --rm redis redis-cli"
-# alias mysql="docker run -it --net=host --rm mysql mysql"
-
 docker_remove_exited () {
   docker rm $(docker ps -f="status=exited" -q)
 }
--- shell/env.sh
+++ shell/env.sh
@@ -10,18 +10,6 @@ export GEM_EDITOR="mate"
 export LC_ALL="en_US.UTF-8"
 export LANG="en_US.UTF-8"
 
-# GCC 4.2 via Homebrew
-# export CC=/usr/local/bin/gcc-4.2
-
-# Support /opt install of zsh
-if [ -d "/opt/zsh/bin" ]; then
-  path_prepend "/opt/zsh/bin"
-fi
-
-# Android SDK
-export ANDROID_SDK_ROOT="/usr/local/Cellar/android-sdk/r20.0.1"
-export ANDROID_HOME="/usr/local/opt/android-sdk"
-
 # ensure bin and sbin paths from /usr/local are in PATH
 path_add_after "/usr/local/sbin" "/usr/local/bin"
 
