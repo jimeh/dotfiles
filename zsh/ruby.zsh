8e2438db6bf68397c7f5bc3dcd2f07c1501f2b04
Jim Myhrberg <contact@jimeh.me>
2019-02-01 16:13:04 +0000

    Install both bundler 1.x and 2.x by default

--- zsh/ruby.zsh
+++ zsh/ruby.zsh
@@ -5,7 +5,8 @@
 install_ruby_global_packages () {
   gem install --no-document \
       brakeman \
-      bundler \
+      'bundler:~> 2.0' \
+      'bundler:~> 1.0' \
       bundler-audit \
       foreman \
       lunchy \
