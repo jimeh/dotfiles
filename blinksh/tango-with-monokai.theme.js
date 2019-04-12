1ba49c6d7ed682879373228f1efdeb05a39033ef
Jim Myhrberg <contact@jimeh.me>
2019-04-12 21:24:15 +0100

    Add custom tango-with-monokai blink shell theme
    Colors are based on those in my Alacritty config, which were at one
    point extracted from iTerm2's preferences GUI.

---
+++ blinksh/tango-with-monokai.theme.js
@@ -0,0 +1,24 @@
+// Tango with Monokai theme
+
+term_set("cursor-color", "#f5f5f5");
+term_set("cursor-blink", false);
+term_set("foreground-color", "#ffffff");
+term_set("background-color", "#000000");
+term_set("color-palette-overrides", [
+  "#000000", // black
+  "#cf5041", // red
+  "#8bba48", // green
+  "#c3a643", // yellow
+  "#37689f", // blue
+  "#9b589d", // magenta
+  "#69b8cb", // cyan
+  "#cccccc", // white
+  "#686a66", // bright black
+  "#ed3a2c", // bright red
+  "#96cf4d", // bright green
+  "#fdeb61", // bright yellow
+  "#5696d5", // bright blue
+  "#9d6aa5", // bright magenta
+  "#75cee3", // bright cyan
+  "#f5f5f5" // bright white
+]);
