542922cde5f3f34d461c1e6e09b50a9591742ccb
Jim Myhrberg <contact@jimeh.me>
2020-05-03 16:01:56 +0100

    Add ctop shell function to run it via docker if ctop binary not found

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -6,6 +6,14 @@ alias d="docker"
 alias dc="docker-compose"
 alias co="docker-compose"
 
+if ! (( $+commands[ctop] )); then
+  ctop() {
+    docker run --rm -ti --name=ctop-$RANDOM \
+           --volume /var/run/docker.sock:/var/run/docker.sock:ro \
+           quay.io/vektorlab/ctop:latest
+  }
+fi
+
 docker_remove_exited () {
   docker rm "$(docker ps -f='status=exited' -q)"
 }
