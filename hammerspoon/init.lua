2ff0508f132c5c4ead50d9e1c2c39b033c07147c
Jim Myhrberg <contact@jimeh.me>
2020-05-06 20:40:19 +0100

    Update PushToTalk spoon to check list of apps when starting
    These changes have been submitted as a PR against the main Spoons
    repository here: https://github.com/Hammerspoon/Spoons/pull/169

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -30,6 +30,7 @@ spoon.HeadphoneAutoPause:start()
 
 -- Enable push-to-talk microphone functionality when specific apps are running.
 hs.loadSpoon('PushToTalk')
+spoon.PushToTalk.detect_on_start = true
 spoon.PushToTalk.app_switcher = { ['TeamSpeak 3'] = 'push-to-talk' }
 spoon.PushToTalk:start()
 
