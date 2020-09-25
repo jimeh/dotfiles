2212138288815cb04292e891cf4b25852c4b7a7f
Jim Myhrberg <contact@jimeh.me>
2020-09-25 12:25:52 +0100

    chore(emacs): update emacs shell setup

--- zsh/emacs.zsh
+++ zsh/emacs.zsh
@@ -5,8 +5,9 @@
 # macOS systems
 if [[ "$OSTYPE" == "darwin"* ]]; then
   if [ -f "/Applications/Emacs.app/Contents/MacOS/Emacs" ]; then
-    alias emacsgui="env TERM=screen-24bit /Applications/Emacs.app/Contents/MacOS/Emacs"
-    alias emacs="env TERM=screen-24bit /Applications/Emacs.app/Contents/MacOS/Emacs -nw"
+    export EMACS="/Applications/Emacs.app/Contents/MacOS/Emacs"
+    alias emacsgui="env TERM=screen-24bit $EMACS"
+    alias emacs="env TERM=screen-24bit $EMACS -nw"
   fi
 
   if [ -f "/Applications/Emacs.app/Contents/MacOS/bin/emacsclient" ]; then
@@ -14,7 +15,11 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   fi
 fi
 
+# Linux systems
 if [[ "$OSTYPE" == "linux"* ]]; then
   alias emacs="env TERM=screen-24bit emacs -nw"
   alias emacsclient="env TERM=screen-24bit emacsclient"
 fi
+
+# add doom-emacs' bin directory to path if it exists
+path_prepend "$HOME/.config/doom-emacs/bin"
