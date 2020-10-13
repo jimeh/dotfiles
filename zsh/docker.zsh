63faaef2c0ddc98f9e4ff3be6eb1cdf61f439d72
Jim Myhrberg <contact@jimeh.me>
2020-10-13 11:51:57 +0100

    fix(docker): docker_remove_exited now works again

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -2,15 +2,16 @@
 # Docker Related
 #
 
-alias d="docker"
-alias dc="docker-compose"
-alias co="docker-compose"
-
-docker_remove_exited() {
-  docker rm "$(docker ps -f='status=exited' -q)"
-}
-
 if command-exists docker; then
   zinit ice from'gh-r' as'program' mv'ctop-* -> ctop'
   zinit light bcicen/ctop
 fi
+
+docker_remove_exited() {
+  docker rm $(docker ps -f='status=exited' -q)
+}
+
+alias d="docker"
+alias dc="docker-compose"
+alias co="docker-compose"
+alias dre="docker_remove_exited"
