855c0d9d9458705258ecb50ab1bc9fe80f1d6962
Jim Myhrberg <contact@jimeh.me>
2021-02-02 12:10:10 +0000

    feat(kubernetes): auto-install various tools if kubectl is available

--- zsh/kubernetes.zsh
+++ zsh/kubernetes.zsh
@@ -12,4 +12,19 @@ if command-exists kubectl; then
     unset -f _kubectl
     eval "$(command kubectl completion zsh)"
   }
+
+  zinit ice wait lucid as'program' from'gh-r' mv'kind-* -> kind' \
+    atclone'./kind completion zsh > _kind' atpull'%atclone'
+  zinit light kubernetes-sigs/kind
+
+  zinit ice wait lucid as'program' from'gh-r' \
+    atclone'./flux completion zsh > _flux' atpull'%atclone'
+  zinit light fluxcd/flux2
+
+  zinit ice wait lucid as'program' from'gh-r' \
+    atclone'./kustomize completion zsh > _kustomize' atpull'%atclone'
+  zinit light kubernetes-sigs/kustomize
+
+  zinit ice wait lucid as'program' from'gh-r' mv'kubeseal-* -> kubeseal'
+  zinit light bitnami-labs/sealed-secrets
 fi
