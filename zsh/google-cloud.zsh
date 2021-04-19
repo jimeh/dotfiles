304eea3f5b65e773919fb5c4253edcc25cb8523d
Jim Myhrberg <contact@jimeh.me>
2021-04-19 01:28:46 +0100

    fix(google-cloud): fix setup of gcloud CLI

--- zsh/google-cloud.zsh
+++ zsh/google-cloud.zsh
@@ -2,6 +2,10 @@
 # Google Cloud SDK setup.
 #
 
+if [ -f '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc' ]; then
+  source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
+fi
+
 if [ -f '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc' ]; then
   source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
 fi
