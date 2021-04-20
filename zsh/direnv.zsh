ff44a4293eaec862fc36ff46494d02eadd3d9f13
Jim Myhrberg <contact@jimeh.me>
2021-04-20 01:14:47 +0100

    chore(direnv): rename zinitrc file

--- zsh/direnv.zsh
+++ zsh/direnv.zsh
@@ -3,6 +3,6 @@
 #
 
 zinit ice wait lucid from'gh-r' as'program' mv'direnv* -> direnv' \
-  atclone'./direnv hook zsh > zhook.zsh' atpull'%atclone' \
-  pick'direnv' src='zhook.zsh'
+  atclone'./direnv hook zsh > .zinitrc.zsh' atpull'%atclone' \
+  pick'direnv' src='.zinitrc.zsh'
 zinit light direnv/direnv
