746b7a79a1dad514e8bbc6bf37a53207f5685b27
Jim Myhrberg <contact@jimeh.me>
2021-04-20 01:18:14 +0100

    chore(tmux): simplify tmuxifier setup loading it in the background

--- zsh/tmux.zsh
+++ zsh/tmux.zsh
@@ -11,39 +11,15 @@ alias tmm="tmn -s main"
 
 # Tmuxifier
 
-zinit ice as'program' pick'bin/tmuxifier' from'gh'
+zinit ice wait lucid as'program' pick'bin/tmuxifier' from'gh' \
+  atclone'./bin/tmuxifier init - > .zinitrc.zsh' atpull'%atclone' \
+  src='.zinitrc.zsh'
 zinit light jimeh/tmuxifier
 
 alias m="tmuxifier"
 alias ms="tmuxifier load-session"
 alias mw="tmuxifier load-window"
 alias mm="tmuxifier load-session main"
-
-# lazy-load tmuxifier
-tmuxifier() {
-  load-tmuxifier
-  tmuxifier "$@"
-}
-
-_tmuxifier() {
-  load-tmuxifier
-  _tmuxifier "$@"
-}
-
-compctl -K _tmuxifier tmuxifier
-
-load-tmuxifier() {
-  # unset lazy-load functions
-  unset -f load-tmuxifier _tmuxifier tmuxifier
-
-  if [ -d "$DOTPFILES/tmux-layouts" ]; then
-    export TMUXIFIER_LAYOUT_PATH="$DOTPFILES/tmux-layouts"
-  else
-    export TMUXIFIER_LAYOUT_PATH="$HOME/.tmux-layouts"
-  fi
-
-  eval "$(command tmuxifier init -)"
-}
 
 use-tmuxifier-dev() {
   path_prepend "$HOME/Projects/tmuxifier/bin"
