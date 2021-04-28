8f649514f21102e235b223eef54449a11c9e2c55
Jim Myhrberg <contact@jimeh.me>
2021-04-28 22:10:42 +0100

    feat(docker): disable docker scan / snyk advertisement

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -11,6 +11,10 @@ docker_remove_exited() {
   docker rm $(docker ps -f='status=exited' -q)
 }
 
+# Disable docker scan suggestion/advertisement. It currently prints even when -q
+# is passed to docker build.
+export DOCKER_SCAN_SUGGEST=false
+
 alias d="docker"
 alias dc="docker compose"
 alias co="docker compose"
