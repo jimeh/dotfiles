30f0a7d953b0fde5e8a63878270510e3ff4f6e42
Jim Myhrberg <contact@jimeh.me>
2022-02-22 19:16:52 +0000

    feat(zsh): install shfmt via zinit

--- zsh/linux.zsh
+++ zsh/linux.zsh
@@ -9,3 +9,7 @@ alias o="xdg-open"
 export TERM="xterm-256color"
 
 path_append "/opt/tigervnc/bin"
+
+zinit ice wait lucid as'program' from'gh-r' mv'shfmt* -> shfmt' \
+  atclone'./shfmt completions zsh > _shfmt' atpull'%atclone'
+zinit light mvdan/sh
