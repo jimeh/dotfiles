b76ea17bd69049551ececae1e32ff83300a7410d
Jim Myhrberg <contact@jimeh.me>
2022-04-20 22:53:15 +0100

    feat(userscripts/kagi-for-safer): minor update/improvement

--- userscripts/kagi-for-safari.user.js
+++ userscripts/kagi-for-safari.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Kagi Search (DuckDuckGo redirect)
 // @description Redirects DuckDuckGo searches to Kagi.com. Only relevant for desktop Safari users.
-// @version     0.0.1
+// @version     0.0.2
 // @namespace   jimeh.me
 // @downloadURL https://github.com/jimeh/dotfiles/blob/main/userscripts/kagi-for-safari.user.js
 // @run-at      document-start
@@ -9,11 +9,13 @@
 // ==/UserScript==
 (function () {
   if (
+    navigator.vendor.match(/apple/i) && // Only activate in Safari.
     window.location.hostname == "duckduckgo.com" &&
     window.location.pathname == "/"
   ) {
     let q = (new URL(window.location)).searchParams.get("q");
     if (q) {
+      console.log("Redirecting DuckDuckGo search to Kagi.com");
       window.location.href = "https://kagi.com/search?q=" + q;
     }
   }
