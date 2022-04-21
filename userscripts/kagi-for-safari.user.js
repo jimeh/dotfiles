a5671fca28ecb0416676b2ce232ea1e8ef7f1788
Jim Myhrberg <contact@jimeh.me>
2022-04-21 02:11:45 +0100

    chore(userscripts/kagi-for-safari): bump version

--- userscripts/kagi-for-safari.user.js
+++ userscripts/kagi-for-safari.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kagi for Safari (DuckDuckGo redirect)
 // @description  Redirects DuckDuckGo searches to Kagi.com. Only relevant for desktop Safari users.
-// @version      0.0.4
+// @version      0.0.5
 // @namespace    jimeh.me
 // @downloadURL  https://github.com/jimeh/dotfiles/raw/main/userscripts/kagi-for-safari.user.js
 // @updateURL    https://github.com/jimeh/dotfiles/raw/main/userscripts/kagi-for-safari.user.js
