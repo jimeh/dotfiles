08c8ac784d1ae952abb0a02082f8e76bcdcc347e
Jim Myhrberg <contact@jimeh.me>
2023-03-09 23:24:39 +0000

    feat(git): add git-largest-objects helper

---
+++ zsh/git.zsh
@@ -0,0 +1,13 @@
+#
+# git
+#
+
+# Stolen from: https://stackoverflow.com/a/42544963
+git-largest-objects() {
+  git rev-list --objects --all |
+    git cat-file --batch-check='%(objecttype) %(objectname) %(objectsize) %(rest)' |
+    sed -n 's/^blob //p' |
+    sort --numeric-sort --key=2 |
+    cut -c 1-12,41- |
+    $(command -v gnumfmt || echo numfmt) --field=2 --to=iec-i --suffix=B --padding=7 --round=nearest
+}
