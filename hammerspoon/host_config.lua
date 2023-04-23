fd94bd2774ff7de7c03372332c8b5919d6e9a2e9
Jim Myhrberg <contact@jimeh.me>
2023-04-23 16:27:31 +0100

    chore(hammerspoon): minor refactor to host_config setup
    Allows it to require any file off of disk by using loadfile instead of
    require.

--- hammerspoon/host_config.lua
+++ hammerspoon/host_config.lua
@@ -1,21 +1,17 @@
-local host = require('hs.host')
-
 local function require_file(path)
-  local modulename = string.gsub(path, "/", "."):gsub("%.lua$", "")
-  local ok, module = pcall(require, modulename)
-
-  return (ok and module or nil)
+  local ok, module = pcall(loadfile, path)
+  return (ok and module and module() or nil)
 end
 
 local obj = {}
 
 function obj:init()
-  local hostname = host.localizedName()
-  local conf_file = "hosts/" .. hostname .. ".lua"
+  local hostname = hs.host.localizedName()
+  local conf_file = 'hosts/' .. hostname .. '.lua'
   local hostmod = require_file(conf_file)
 
   if hostmod then
-    print("loading host config: " .. conf_file)
+    print('loading host config: ' .. conf_file)
     hostmod.init()
   end
 end
