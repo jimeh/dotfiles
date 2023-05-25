e24d969a1ed3f5632176cae643bdd4ef5c41f8d0
Jim Myhrberg <contact@jimeh.me>
2023-05-25 02:25:38 +0100

    chore(golang): tidy up an update global package list

--- zsh/golang.zsh
+++ zsh/golang.zsh
@@ -16,26 +16,22 @@ install_go_global_packages() {
   local packages=(
     github.com/akavel/up@latest
     github.com/asciimoo/wuzz@latest
+    github.com/caddyserver/xcaddy/cmd/xcaddy@latest
     github.com/cweill/gotests/...@latest
     github.com/cweill/gotests/gotests@latest
     github.com/erning/gorun@latest
     github.com/go-delve/delve/cmd/dlv@latest
     github.com/golang/mock/mockgen@latest
-    github.com/google/go-jsonnet/cmd/jsonnet@latest
-    github.com/google/go-jsonnet/cmd/jsonnetfmt@latest
     github.com/icholy/gomajor@latest
     github.com/kisielk/errcheck@latest
-    github.com/lighttiger2505/sqls@latest
     github.com/mdempsky/unconvert@latest
     github.com/nametake/golangci-lint-langserver@latest
     github.com/ramya-rao-a/go-outline@latest
     github.com/rogpeppe/godef@latest
     github.com/segmentio/golines@latest
     github.com/uudashr/gopkgs/v2/cmd/gopkgs@latest
-    golang.org/x/lint/golint@latest
     golang.org/x/tools/cmd/godoc@latest
     golang.org/x/tools/cmd/goimports@latest
-    golang.org/x/tools/cmd/guru@latest
     golang.org/x/tools/cmd/stringer@latest
     golang.org/x/tools/gopls@latest
     google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
