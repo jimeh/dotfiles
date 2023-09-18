0b432f137da096b5bdfe7356d69ab254d584cc31
Jim Myhrberg <contact@jimeh.me>
2023-09-18 11:35:11 +0100

    chore(hammerspoon): update global hotkeys

--- hammerspoon/hosts/noct.lua
+++ hammerspoon/hosts/noct.lua
@@ -4,29 +4,22 @@ function obj.init()
   local apptoggle = require('app_toggle')
 
   apptoggle:bind({ 'cmd', 'alt', 'ctrl' }, 'A', { 'Activity Monitor' })
-  apptoggle:bind({ 'cmd', 'ctrl' }, '4', { 'Microsoft Edge' })
+  apptoggle:bind({ 'cmd', 'ctrl' }, '2', { 'ChatGPT' })
+  apptoggle:bind({ 'cmd', 'ctrl' }, '4', { 'FastGPT' })
+  apptoggle:bind({ 'cmd', 'ctrl' }, '5', { 'Microsoft Edge' })
   apptoggle:bind({ 'cmd', 'ctrl' }, 'A', { 'Messages' })
+  apptoggle:bind({ 'cmd', 'ctrl' }, 'B', { 'TablePlus' }, { 'Lens' })
   apptoggle:bind({ 'cmd', 'ctrl' }, 'C', { 'Calendar' })
-  apptoggle:bind({ 'cmd', 'ctrl' }, 'D', { 'Mailplane' })
+  apptoggle:bind({ 'cmd', 'ctrl' }, 'D', { 'Mimestream' }, { 'Mailplane' })
+  apptoggle:bind({ 'cmd', 'ctrl' }, 'E', { 'Emacs', '/Applications/Emacs.app' })
   apptoggle:bind({ 'cmd', 'ctrl' }, 'F', { 'Element Nightly' })
   apptoggle:bind({ 'cmd', 'ctrl' }, 'S', { 'Music' })
   apptoggle:bind({ 'cmd', 'ctrl' }, 'T', { 'Discord PTB' })
-  apptoggle:bind({ 'cmd', 'ctrl' }, 'X', { 'Notion' })
+  apptoggle:bind({ 'cmd', 'ctrl' }, 'X', { 'Notion' }, { 'Obsidian' })
   apptoggle:bind({ 'cmd', 'ctrl' }, 'Z', { 'Slack' })
 
-  apptoggle:bind({ 'cmd', 'ctrl' }, '2',
-    { 'ChatGPT X' },
-    { 'ChatGPT' }
-  )
-  apptoggle:bind({ 'cmd', 'ctrl' }, 'B',
-    { 'TablePlus' },
-    { 'Lens' }
-  )
-
-  apptoggle:bind({ 'cmd', 'ctrl' }, 'E',
-    { 'Emacs', '/Applications/Emacs.app' }
-  )
   apptoggle:bind({ 'cmd', 'ctrl' }, 'W',
+    { 'Code - Insiders', '/Applications/Visual Studio Code - Insiders.app' },
     { 'Code', '/Applications/Visual Studio Code.app' }
   )
 end
