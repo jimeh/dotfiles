2cbd074bace9fc22f49a56177d475d910ffc54ba
Jim Myhrberg <contact@jimeh.me>
2024-05-09 02:23:13 +0100

    feat(tools): install Node-based CLI tools with mise
    Use `mise` to install and manage tools normally installed with `npm`.

--- zsh/nodejs.zsh
+++ zsh/nodejs.zsh
@@ -8,36 +8,6 @@
 
 install_node_global_packages() {
   local npm_packages=(
-    @commitlint/cli
-    @commitlint/config-conventional
-    @mermaid-js/mermaid-cli
-    @prettier/plugin-php
-    @prettier/plugin-ruby
-    @prettier/plugin-xml
-    dockerfile-language-server-nodejs
-    eslint
-    eslint-config-prettier
-    eslint-plugin-prettier
-    eslint_d
-    htmllint-cli
-    httpsnippet
-    js-beautify
-    jsonlint
-    localtunnel
-    markdown-it
-    prettier
-    prettier-plugin-toml
-    prettier-pnp
-    standard-version
-    stylelint
-    tslint
-    typescript
-    typescript-formatter
-    typescript-language-server
-    uuid-cli
-    vscode-css-languageserver-bin
-    vscode-json-languageserver
-    yaml-language-server
   )
 
   npm install -g "${npm_packages[@]}"
