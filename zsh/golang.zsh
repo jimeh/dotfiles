1158360c214baca81826f53f31c2e01b3985247d
Jim Myhrberg <contact@jimeh.me>
2024-05-09 02:21:17 +0100

    feat(tools): install Go-based CLI tools with mise
    Use `mise` to install and manage tools normally installed with
    `go install`.

--- zsh/golang.zsh
+++ zsh/golang.zsh
@@ -14,29 +14,6 @@ list_go_global_packages() {
 
 install_go_global_packages() {
   local packages=(
-    github.com/akavel/up@latest
-    github.com/asciimoo/wuzz@latest
-    github.com/bufbuild/buf-language-server/cmd/bufls@latest
-    github.com/caddyserver/xcaddy/cmd/xcaddy@latest
-    github.com/erning/gorun@latest
-    github.com/go-delve/delve/cmd/dlv@latest
-    github.com/golang/mock/mockgen@latest
-    github.com/icholy/gomajor@latest
-    github.com/kisielk/errcheck@latest
-    github.com/mdempsky/unconvert@latest
-    github.com/nametake/golangci-lint-langserver@latest
-    github.com/ramya-rao-a/go-outline@latest
-    github.com/rogpeppe/godef@latest
-    github.com/segmentio/golines@latest
-    github.com/uudashr/gopkgs/v2/cmd/gopkgs@latest
-    golang.org/x/tools/cmd/godoc@latest
-    golang.org/x/tools/cmd/goimports@latest
-    golang.org/x/tools/cmd/stringer@latest
-    golang.org/x/tools/gopls@latest
-    golang.org/x/vuln/cmd/govulncheck@latest
-    google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
-    google.golang.org/protobuf/cmd/protoc-gen-go@latest
-    mvdan.cc/gofumpt@latest
   )
 
   for package in "${packages[@]}"; do
