62f525177433f9bdfd6cd8838d9c123298cfb3b8
Jim Myhrberg <jim@krystal.io>
2025-02-14 21:38:17 +0000

    feat(golang/packages): add gofumpt to global package list

--- zsh/golang.zsh
+++ zsh/golang.zsh
@@ -32,6 +32,7 @@ install_go_global_packages() {
     golang.org/x/vuln/cmd/govulncheck@latest
     google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
     google.golang.org/protobuf/cmd/protoc-gen-go@latest
+    mvdan.cc/gofumpt@latest
   )
 
   for package in "${packages[@]}"; do
