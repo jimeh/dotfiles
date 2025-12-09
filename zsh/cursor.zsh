c2ab0a936230f696ca6525dfb904ef7c040b6d0a
Jim Myhrberg <contact@jimeh.me>
2025-12-09 21:58:21 +0000

    feat(zsh): set cursor as default editor if available

--- zsh/cursor.zsh
+++ zsh/cursor.zsh
@@ -8,3 +8,7 @@ if command-exists cursor-agent; then
   setup-completions cursor-agent "$(command-path cursor-agent)" \
     cursor-agent shell-integration zsh
 fi
+
+if command-exists cursor; then
+  export EDITOR="cursor -w"
+fi
