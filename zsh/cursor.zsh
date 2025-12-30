4756212c329e6640092a2517b99e5ed04ce91af8
Jim Myhrberg <contact@jimeh.me>
2025-12-30 13:52:47 +0000

    feat(zsh/cursor): set cursor as default editor and update alias for cursor command

--- zsh/cursor.zsh
+++ zsh/cursor.zsh
@@ -3,12 +3,12 @@
 #
 
 if command-exists cursor-agent; then
-  alias cu="cursor-agent"
-
   setup-completions cursor-agent "$(command-path cursor-agent)" \
     cursor-agent shell-integration zsh
 fi
 
 if command-exists cursor; then
   export EDITOR="cursor -w"
+  alias cu="cursor"
+  alias e="cursor -w"
 fi
